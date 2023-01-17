ad3d03d4c6e3d19d4aef84cb65a3c28b9ca73130
Jérémy Luu <73641462+Jeyluu@users.noreply.github.com>
2023-01-17 18:03:11 +0100

    Placing footer at mainPage

--- src/Pages/MainPage.tsx
+++ src/Pages/MainPage.tsx
@@ -1,14 +1,36 @@
-import { memo, useCallback, useState } from 'react'
+import { memo, useCallback, useMemo, useState } from 'react'
+import Footer from '../components/Footer/Footer'
 import Introduction from '../components/Introduction/Introduction'
 import ProjectsSection from '../components/ProjectsSection/ProjectsSection'
 import { useProjects } from '../Hooks/useProjects'
 import { ProjectsI } from '../Types/Project'
+import LinkedIn from '../Assets/Icons/linkedin.png'
+import Github from '../Assets/Icons/github.png'
 
 import './MainPages.css'
+
+const footerIcon = [
+  {
+    alt: 'LinkedIn',
+    link: 'https://www.linkedin.com/in/j%C3%A9r%C3%A9my-luu-820419124/',
+    icon: LinkedIn,
+  },
+  {
+    alt: 'Github',
+    link: 'https://github.com/Jeyluu',
+    icon: Github,
+  },
+]
+
+const copyRight = 'contact'
 
 function MainPage() {
   const projectsDatas = useProjects()
   const [focusedCard, setFocusedCard] = useState<number>(-1)
+
+  const copyRightStyle = useMemo(() => {
+    return copyRight.toUpperCase()
+  }, [])
 
   const focusedACard = useCallback(
     (project: ProjectsI) => {
@@ -39,6 +61,11 @@ function MainPage() {
           )
         })}
       </section>
+      <Footer
+        filigrane={copyRightStyle}
+        copyright={'2023 Jérémy Luu - Développeur Front-End'}
+        icons={footerIcon}
+      />
     </>
   )
 }
