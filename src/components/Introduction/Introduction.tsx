38f023f38aced0f7c68f2203f5093a3795fd8048
Jérémy Luu <73641462+Jeyluu@users.noreply.github.com>
2023-01-17 22:56:58 +0100

    1st step of techno used and langage. Next step list dynamic with square color

--- src/components/Introduction/Introduction.tsx
+++ src/components/Introduction/Introduction.tsx
@@ -4,24 +4,40 @@ import './Introduction.css'
 
 function Introduction() {
   return (
-    <>
-      <div id="img-container">
-        <img id="presentation-img" src={FakeImage} alt="FakeImg" />
+    <div id="intro-container">
+      <div id="description-container">
+        <div id="img-container">
+          <img id="presentation-img" src={FakeImage} alt="FakeImg" />
+        </div>
+
+        <div id="description-bloc">
+          <h1>Présentation</h1>
+          <p>
+            Hello,
+            <span>
+              Je suis Jérémy et je suis développeur logiciel front-end. Je suis
+              un passionné de technologies,très curieux et soif d'apprendre. Je
+              continu à me développer de jour en jour.
+            </span>
+          </p>
+        </div>
       </div>
 
-      <div id="description-bloc">
-        <h1>Présentation</h1>
-        <p>
-          Hello,
-          <span>
-            Je suis Jérémy et je suis développeur logiciel front-end. Je suis un
-            passionné de technologies,très curieux et soif d'apprendre. Je
-            continu à me développer de jour en jour.
-          </span>
-        </p>
-        
+      <div id="stack">
+
+          <ul id="stack-list">
+            <li>Html</li>
+            <li>Css</li>
+            <li>Javascript</li>
+          </ul>
+
+        <div id="stack-container">
+          <span className="stack-square"></span>
+          <span className="stack-square"></span>
+          <span className="stack-square"></span>
+        </div>
       </div>
-    </>
+    </div>
   )
 }
 
